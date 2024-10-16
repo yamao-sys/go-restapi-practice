9f29fa4e486ce0ceccee8c4593d0338efd5a9975
yamao-sys <yamaopanku@gmail.com>
2024-10-17 08:57:52 +0900

    エントリポイントにTodoのDI追加

--- app/main.go
+++ app/main.go
@@ -21,18 +21,23 @@ func main() {
 
 	// repository
 	userRepository := repositories.NewUserRepository(dbCon)
+	todoRepository := repositories.NewTodoRepository(dbCon)
 
 	// service
 	authService := services.NewAuthService(userRepository)
+	todoService := services.NewTodoService(todoRepository)
 
 	// controller
 	authController := controllers.NewAuthController(authService)
+	todoController := controllers.NewTodoController(todoService, authService)
 	authRouter := routers.NewAuthRouter(authController)
+	todoRouter := routers.NewTodoRouter(todoController)
 
 	// router
 	r := gin.Default()
 
 	r.GET("/", controllers.TopPage)
 	authRouter.SetRouting(r)
+	todoRouter.SetRouting(r)
 	r.Run(":" + strconv.Itoa(config.Config.ServerPort))
 }
