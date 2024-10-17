8270a9eed6ebb9f1779d045b097333fd03767d10
yamao-sys <yamaopanku@gmail.com>
2024-10-17 10:04:35 +0900

    Todoコントローラを読みやすくするため、なくても良い改行をなくす

--- app/controllers/todo.controller.go
+++ app/controllers/todo.controller.go
@@ -28,152 +28,110 @@ func NewTodoController(todoService services.TodoService, authService services.Au
 func (todoController *todoController) Create(ctx *gin.Context) {
 	user, err := todoController.authService.GetAuthUser(ctx)
 	if err != nil {
-		ctx.JSON(http.StatusUnauthorized, gin.H{
-			"error": "unauthorized error",
-		})
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized error"})
 		return
 	}
 
 	result := todoController.todoService.CreateTodo(ctx, user.ID)
 
 	if result.Error == nil {
-		ctx.JSON(http.StatusOK, gin.H{
-			"todo": result.Todo,
-		})
+		ctx.JSON(http.StatusOK, gin.H{"todo": result.Todo})
 		return
 	}
 
 	switch result.ErrorType {
 	case "internalServerError":
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"error": result.Error,
-		})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": result.Error})
 	case "validationError":
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error": utils.CoordinateValidationErrors(result.Error),
-		})
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": utils.CoordinateValidationErrors(result.Error)})
 	}
 }
 
 func (todoController *todoController) Index(ctx *gin.Context) {
 	user, err := todoController.authService.GetAuthUser(ctx)
 	if err != nil {
-		ctx.JSON(http.StatusUnauthorized, gin.H{
-			"error": "unauthorized error",
-		})
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized error"})
 		return
 	}
 
 	result := todoController.todoService.FetchTodosList(ctx, user.ID)
 
 	if result.Error == nil {
-		ctx.JSON(http.StatusOK, gin.H{
-			"todos": result.Todos,
-		})
+		ctx.JSON(http.StatusOK, gin.H{"todos": result.Todos})
 		return
 	}
 
 	switch result.ErrorType {
 	case "notFound":
-		ctx.JSON(http.StatusNotFound, gin.H{
-			"error": result.Error,
-		})
+		ctx.JSON(http.StatusNotFound, gin.H{"error": result.Error})
 	case "internalServerError":
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"error": result.Error,
-		})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": result.Error})
 	}
 }
 
 func (todoController *todoController) Show(ctx *gin.Context) {
 	user, err := todoController.authService.GetAuthUser(ctx)
 	if err != nil {
-		ctx.JSON(http.StatusUnauthorized, gin.H{
-			"error": "unauthorized error",
-		})
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized error"})
 		return
 	}
 
 	result := todoController.todoService.FetchTodo(ctx, user.ID)
 
 	if result.Error == nil {
-		ctx.JSON(http.StatusOK, gin.H{
-			"todo": result.Todo,
-		})
+		ctx.JSON(http.StatusOK, gin.H{"todo": result.Todo})
 		return
 	}
 
 	switch result.ErrorType {
 	case "notFound":
-		ctx.JSON(http.StatusNotFound, gin.H{
-			"error": result.Error,
-		})
+		ctx.JSON(http.StatusNotFound, gin.H{"error": result.Error})
 	case "internalServerError":
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"error": result.Error,
-		})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": result.Error})
 	}
 }
 
 func (todoController *todoController) Update(ctx *gin.Context) {
 	user, err := todoController.authService.GetAuthUser(ctx)
 	if err != nil {
-		ctx.JSON(http.StatusUnauthorized, gin.H{
-			"error": "unauthorized error",
-		})
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized error"})
 		return
 	}
 
 	result := todoController.todoService.UpdateTodo(ctx, user.ID)
 
 	if result.Error == nil {
-		ctx.JSON(http.StatusOK, gin.H{
-			"todo": result.Todo,
-		})
+		ctx.JSON(http.StatusOK, gin.H{"todo": result.Todo})
 		return
 	}
 
 	switch result.ErrorType {
 	case "validationError":
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error": utils.CoordinateValidationErrors(result.Error),
-		})
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": utils.CoordinateValidationErrors(result.Error)})
 	case "notFound":
-		ctx.JSON(http.StatusNotFound, gin.H{
-			"error": result.Error,
-		})
+		ctx.JSON(http.StatusNotFound, gin.H{"error": result.Error})
 	case "internalServerError":
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"error": result.Error,
-		})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": result.Error})
 	}
 }
 
 func (todoController *todoController) Delete(ctx *gin.Context) {
 	user, err := todoController.authService.GetAuthUser(ctx)
 	if err != nil {
-		ctx.JSON(http.StatusUnauthorized, gin.H{
-			"error": "unauthorized error",
-		})
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized error"})
 		return
 	}
 	result := todoController.todoService.DeleteTodo(ctx, user.ID)
 
 	if result.Error == nil {
-		ctx.JSON(http.StatusOK, gin.H{
-			"result": "delete todo(ID: " + ctx.Param("id") + ") successfully",
-		})
+		ctx.JSON(http.StatusOK, gin.H{"result": "delete todo(ID: " + ctx.Param("id") + ") successfully"})
 		return
 	}
 
 	switch result.ErrorType {
 	case "notFound":
-		ctx.JSON(http.StatusNotFound, gin.H{
-			"error": result.Error,
-		})
+		ctx.JSON(http.StatusNotFound, gin.H{"error": result.Error})
 	case "internalServerError":
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"error": result.Error,
-		})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": result.Error})
 	}
 }
